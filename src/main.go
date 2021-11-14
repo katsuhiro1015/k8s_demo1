3fef21cea37b590e6530a5562421779b58b9d8b5
yamanaka <yamanaka.mb@gmail.com>
2021-11-15 00:25:56 +0900

    dapr

--- src/main.go
+++ src/main.go
@@ -9,15 +9,16 @@ import (
 func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello Usagisan/")
 	//resp, err := http.Get("http://localhost:3500/v1.0/invoke/demo2/method/hello")
-	//resp, err := http.Get("http://demo2:8081/hello")
-	req, _ := http.NewRequest("GET", "http://demo2:8082/hello", nil)
-	req.Header.Set("HTTP2_HEADER", "test")
+	//req, _ := http.NewRequest("GET", "http://demo2:8082/demo", nil)
+	req, _ := http.NewRequest("POST", "http://localhost:3500/v1.0/publish/pubsub/demo", nil)
+	//req.Header.Set("HTTP2_HEADER", "test")
 
 	client := new(http.Client)
 	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Fprint(writer, resp.StatusCode)
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
